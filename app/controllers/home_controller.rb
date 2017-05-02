e181c80d8cb8e1338837146e770a1599375d109e
Raoni Boaventura <raonidonordeste@gmail.com>
2017-05-02 16:18:51 +0200

    Removed and added a bunch of file

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @months = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
-    @days = ["Montag", "  Dienstag", "  Mittwoch", "Donnerstag", "Freitag", " Samstag", "Sonntag"] 
-  end
-
-  def show
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-
-  root 'home#index'
-
-  get 'home/show'
-
-  get "contact", to: "contact#index"
-
-  get "contact/about", to: "contact#about"
+  root 'movies#index'
 end
